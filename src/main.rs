0dc4540bdb84f6b9150d7929574fbe07a79f7097
ndndc <chxbcjf@gmail.com>
2025-12-05 00:14:28 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
-mod stagescan_mac;
-#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-mod stagescan_mac_arm;
-#[cfg(target_os = "windows")]
-mod stagescan;
-
 use std::fs;
 use std::path::PathBuf;
-
-#[cfg(target_os = "macos")]
-use std::process::Command;
-
 use std::time::Instant;
 use clap::Parser;
-
-#[cfg(windows)]
 use winreg::{enums::HKEY_CLASSES_ROOT, RegKey};
 
 #[derive(Parser)]
@@ -24,109 +11,46 @@ struct Cli {
     output: Option<PathBuf>,
 }
 
-#[cfg(target_os = "macos")]
-fn codesign(path: &PathBuf) {
-    let status = Command::new("codesign")
-        .arg("--force")
-        .arg("--sign")
-        .arg("-")
-        .arg(path)
-        .status()
-        .expect("Codesign command failed, studio may crash/not work properly due to codesigning failing");
+fn patch(input: &PathBuf, output: &PathBuf) {
+    let input_data = match fs::read(input) {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("Failed to read input file {:?}: {}", input, e);
+            std::process::exit(1);
+        }
+    };
 
-    if !status.success() {
-        eprintln!("Codesign command failed, studio may crash/not work properly due to codesigning failing");
-        std::process::exit(1);
-    }
-}
-
-fn patch(input: Option<PathBuf>, output: &PathBuf) {
-    let input_path = input.as_ref().unwrap();
-    let input = fs::read(input_path).unwrap();
-
-    #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-    {
-        let mut input = input;
-        let now = Instant::now();
-        stagescan_mac_arm::start(&mut input, output);
-        println!("Patched in {:?}. Codesigning", now.elapsed());
-
-        let codesigning_now = Instant::now();
-        codesign(output);
-        println!("Codesigning finished in {:?}.", codesigning_now.elapsed());
-    }
-
-    #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
-    {
-        let now = Instant::now();
-        stagescan_mac::start(input, output);
-        println!("Patched in {:?}. Codesigning", now.elapsed());
-
-        let codesigning_now = Instant::now();
-        codesign(output);
-        println!("Codesigning finished in {:?}.", codesigning_now.elapsed());
-    }
-
-    #[cfg(not(target_os = "macos"))]
-    {
-        let now = Instant::now();
-        stagescan::start(input, output);
-        println!("Patched in {:?}.", now.elapsed());
-    }
+    let now = Instant::now();
+    stagescan::start(input_data, output);
+    println!("Patched in {:?}", now.elapsed());
 }
 
 fn main() {
     let Cli { mut input, output } = Cli::parse();
-    #[cfg(target_os = "macos")]
-    {
-        input = input.or_else(|| {
-            Some(PathBuf::from(
-                "/Applications/RobloxStudio.app/Contents/MacOS/RobloxStudio",
-            ))
-        });
-    }
-    #[cfg(target_os = "windows")]
-    {
-        if input.is_none() {
-            let path: String = RegKey::predef(HKEY_CLASSES_ROOT)
-                .open_subkey("roblox-studio")
-                .unwrap()
-                .open_subkey("DefaultIcon")
-                .unwrap()
-                .get_value("")
-                .unwrap();
 
-            input = input.or_else(|| Some(PathBuf::from(path)));
-        }
-    }
-    let output = if cfg!(debug_assertions) {
-        #[cfg(target_os = "macos")]
+    if input.is_none() {
+        let path = match RegKey::predef(HKEY_CLASSES_ROOT)
+            .open_subkey("roblox-studio")
+            .and_then(|k| k.open_subkey("DefaultIcon"))
+            .and_then(|k| k.get_value::<String, _>(""))
         {
-            output.unwrap_or_else(|| {
-                input
-                    .as_ref()
-                    .unwrap()
-                    .with_file_name("RobloxStudioBeta_INTERNAL")
-            })
-        }
-        #[cfg(target_os = "windows")]
-        {
-            output.unwrap_or_else(|| {
-                input
-                    .as_ref()
-                    .unwrap()
-                    .with_file_name("RobloxStudioBeta_INTERNAL.exe")
-            })
-        }
-    } else {
-        output.unwrap_or_else(|| input.as_ref().unwrap().clone())
-    };
-    #[cfg(target_os = "macos")]
-    {
-        if Command::new("codesign").arg("--version").output().is_err() { // can codesign not exist in some cases? no idea, will keep here though
-            eprintln!("Error: codesign not found. Please run `xcode-select --install` first.");
-            std::process::exit(1);
-        }
+            Ok(p) => p,
+            Err(_) => {
+                eprintln!("Could not find Roblox Studio path in registry. Please provide --input manually.");
+                std::process::exit(1);
+            }
+        };
+
+        input = Some(PathBuf::from(path));
     }
+
+    let input = input.as_ref().unwrap();
+    if !input.exists() {
+        eprintln!("Input file {:?} does not exist.", input);
+        std::process::exit(1);
+    }
+
+    let output = output.unwrap_or_else(|| input.with_file_name("RobloxStudioBeta_INTERNAL.exe"));
+
     patch(input, &output);
 }
